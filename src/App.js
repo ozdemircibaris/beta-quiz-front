e029b144be64de32d67ad07f849f53e5a9550408
Barış ÖZDEMİRCİ <ozdemircibaris@gmail.com>
2020-10-01 10:30:35 +0300

    video player configration

--- src/App.js
+++ src/App.js
@@ -1,36 +1,75 @@
 import React, { Component } from 'react';
 import YouTube from 'react-youtube';
-
+const opts = {
+  height: '390',
+  width: '640',
+  controls: 0,
+  playerVars: {
+    controls: 0,
+    disablekb: true,
+  // https://developers.google.com/youtube/player_parameters
+  },
+};
 export default class App extends Component {
   state = {
     timerStatus: false,
-    timer: 0
+    secsTimer: 0,
+    milliSecstimer: 0,
+    videoRefs: null,
+    formStatus: false,
+    nextStatus: false,
   }
-  tick = () => this.setState({ timer: this.state.timer + 1 })
+  milliSecsTick = () => this.setState({ secsTimer: this.state.milliSecstimer / 100, milliSecstimer: this.state.milliSecstimer + 1 })
 
-  startTimer = () => {
-    const { timerStatus, timer } = this.state;
-    if(timerStatus == false) {
-      this.interval = setInterval(this.tick, 1000);
-      this.setState({ timerStatus: true })
+  startTimer = (e, status) => {
+    const { timerStatus, videoRefs, nextStatus } = this.state;
+    console.log('status', status)
+    let videoCurrentTime = e != null ? (e.target.playerInfo.currentTime).toFixed(1):null;
+    this.setState({ secsTimer: videoCurrentTime })
+
+    if(status == "play") {
+      this.milliSecsInterval = setInterval(this.milliSecsTick, 10);
+      videoRefs.playVideo()
+    } else if(status == "pause") {
+      clearInterval(this.milliSecsInterval);
+      videoRefs.pauseVideo()
     } else {
-      clearInterval(this.interval);
-      this.setState({ timerStatus: false })
+      // videoRefs.pauseVideo()
+    }
+  }
+
+  componentDidUpdate(props) {
+    const { secsTimer, milliSecstimer } = this.state;
+    if(secsTimer == "2") {
+      this.startTimer(null, "pause")
+      this.setState({ formStatus: true })
     }
   }
 
   render() {
-    const { timerStatus, timer } = this.state;
-    console.log('timer', timer)
+    const { secsTimer, milliSecstimer, videoRefs, formStatus } = this.state;
+    console.log('videoRefs', videoRefs)
     return (
       <div>
-        <YouTube videoId="e5QEAI_O2IA" opts={opts} onReady={this._onReady} />;
-        <h1> timer: {timer} </h1>
-        <button
-          onClick={this.startTimer}
-          style={{ margin: 50 }}>
-            {timerStatus == false ? "Start":"Pause"}
-        </button>
+        <YouTube videoId="e5QEAI_O2IA"
+          opts={opts}
+
+          onPlay={(e) => this.startTimer(e, "play")}
+          onPause={(e) => this.startTimer(e, "pause")}
+          // onStateChange={(e) => console.log('e.target', e.target.playerInfo.currentTime)}
+          onReady={(e) => this.setState({ videoRefs: e.target })} />
+        <h1> secs timer: {secsTimer} </h1>
+        <h1> milli secs timer: {milliSecstimer} </h1>
+
+        {
+          formStatus == true ? <h1> form çıkacakkk </h1>:<h1> çıkmıcakkk </h1>
+        }
+        {
+          formStatus == true ? <button onClick={() => {
+            this.setState({ nextStatus: true })
+            this.startTimer(null, "play")
+          }}> devam et paşa </button>:null
+        }
       </div>
     )
   }
