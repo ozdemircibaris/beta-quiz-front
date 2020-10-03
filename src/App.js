b1b13804fbf57c347478eb6c88c0bef46d9d26f9
Barış ÖZDEMİRCİ <ozdemircibaris@gmail.com>
2020-10-03 09:47:21 +0300

    added video player

--- src/App.js
+++ src/App.js
@@ -1,74 +1,37 @@
 import React, { Component } from 'react';
-import YouTube from 'react-youtube';
-const opts = {
-  height: '390',
-  width: '640',
-  controls: 0,
-  playerVars: {
-    controls: 0,
-    disablekb: true,
-  // https://developers.google.com/youtube/player_parameters
-  },
-};
+import video from './videos/serefsizGS.mp4';
+import ReactPlayer from 'react-player'
+
 export default class App extends Component {
   state = {
-    timerStatus: false,
-    secsTimer: 0,
-    milliSecstimer: 0,
-    videoRefs: null,
-    formStatus: false,
-    nextStatus: false,
+    videoStatus: false,
+    formStatus: false
   }
-  milliSecsTick = () => this.setState({ secsTimer: this.state.milliSecstimer / 100, milliSecstimer: this.state.milliSecstimer + 1 })
-
-  startTimer = (e, status) => {
-    const { timerStatus, videoRefs, nextStatus } = this.state;
-    console.log('status', status)
-    let videoCurrentTime = e != null ? (e.target.playerInfo.currentTime).toFixed(1):null;
-    this.setState({ secsTimer: videoCurrentTime })
-
-    if(status == "play") {
-      this.milliSecsInterval = setInterval(this.milliSecsTick, 10);
-      videoRefs.playVideo()
-    } else if(status == "pause") {
-      clearInterval(this.milliSecsInterval);
-      videoRefs.pauseVideo()
-    } else {
-      // videoRefs.pauseVideo()
+  onLoadForm = (playedSeconds) => {
+    let toFixedSeconds = playedSeconds.toFixed(1)
+    if(toFixedSeconds === "3.0") {
+      this.setState({ videoStatus: false, formStatus: true })
     }
   }
-
-  componentDidUpdate(props) {
-    const { secsTimer, milliSecstimer } = this.state;
-    if(secsTimer == "2") {
-      this.startTimer(null, "pause")
-      this.setState({ formStatus: true })
-    }
-  }
-
   render() {
-    const { secsTimer, milliSecstimer, videoRefs, formStatus } = this.state;
-    console.log('videoRefs', videoRefs)
+    const { videoStatus, formStatus } = this.state;
     return (
       <div>
-        <YouTube videoId="e5QEAI_O2IA"
-          opts={opts}
-
-          onPlay={(e) => this.startTimer(e, "play")}
-          onPause={(e) => this.startTimer(e, "pause")}
-          // onStateChange={(e) => console.log('e.target', e.target.playerInfo.currentTime)}
-          onReady={(e) => this.setState({ videoRefs: e.target })} />
-        <h1> secs timer: {secsTimer} </h1>
-        <h1> milli secs timer: {milliSecstimer} </h1>
-
+        <ReactPlayer
+          ref={ref => this.videoRef = ref}
+          url={video}
+          progressInterval={100}
+          playing={videoStatus}
+          onProgress={(e) => this.onLoadForm(e.playedSeconds)}
+          />
+        { videoStatus === false && formStatus === false ? <button style={{ margin: 10}} onClick={() =>this.setState({ videoStatus: true })}>start</button>:null}
         {
-          formStatus == true ? <h1> form çıkacakkk </h1>:<h1> çıkmıcakkk </h1>
-        }
-        {
-          formStatus == true ? <button onClick={() => {
-            this.setState({ nextStatus: true })
-            this.startTimer(null, "play")
-          }}> devam et paşa </button>:null
+          formStatus === true ?
+          <div>
+            <h1> öğtmenim böğün burda form çıkacak </h1>
+            <button onClick={() => this.setState({videoStatus: true, formStatus: false }) }> devam et </button>
+          </div>
+          :null
         }
       </div>
     )
